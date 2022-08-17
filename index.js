459c668122a024b7af4849138e792128a9d376ff
codinci <dkibau@gmail.com>
2022-08-17 21:00:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
 // Write your code here
+// function multiply(num1, num2) {
+//     if(typeof num1 == 'number' && typeof num2 == 'number') {
+//         const product = num1 * num2;
+//         if(product !== 62) {
+//             console.log('Please input numbers whose product is equal 62');
+//         } else {
+//             console.log(`Thank you for choosing ${num1} and ${num2}`);
+//         }
+//     } else {
+//         console.log("Please enter a valid number");
+//     }
+// }
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 10) + 1;
+console.log(random);
+const num3 = 64;
+const num4 = 10;
+const mod = num3 % num4;
+const max = Math.max(2, 9, 19, 20);
